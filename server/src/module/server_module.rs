2a627c6d1aaf668a7549f0e5189ed12dfe134bdd
fredyakumo <fredyakumo@gmail.com>
2024-08-22 15:57:15 +0800

    Add fetch model info api

--- server/src/module/server_module.rs
+++ server/src/module/server_module.rs
@@ -1,4 +1,8 @@
-use actix_web::{get, HttpResponse, Responder, HttpRequest};
+use std::sync::Arc;
+
+use actix_web::{get, post, HttpRequest, HttpResponse, Responder};
+use actix_web::web::Data;
+use serde::Deserialize;
 
 fn get_ip_info_from_request(_req: &HttpRequest) -> String {
     let addr = _req.peer_addr().expect("unknown addr");
@@ -10,4 +14,16 @@ fn get_ip_info_from_request(_req: &HttpRequest) -> String {
 pub async fn req_process(_req: HttpRequest) -> impl Responder {
     let ip_addr = get_ip_info_from_request(&_req);
     return HttpResponse::Ok().body(format!("Your IP address: {}", ip_addr));
+}
+
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+struct ClassNameModHashList {
+    pub className: String,
+    pub modList: Vec<String>
+}
+
+#[post("/fetchMissingModList")]
+pub async fn fetch_missiong_mod_list(class_name_mod_list: Data<Arc<Vec<ClassNameModHashList>>>) -> impl Responder {
+
 }
